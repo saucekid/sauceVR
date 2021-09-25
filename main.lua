f0038b3cd8a2d31d6cedb1281afae64d07dc7331
saucekid <76892761+saucekid@users.noreply.github.com>
2021-09-25 11:51:20 -0400

    Modules

---
+++ main.lua
@@ -0,0 +1,13 @@
+--=========[Variables]
+local Players = game:GetService("Players");     
+local Lighting = game:GetService("Lighting");
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local ScriptContext = game:GetService("ScriptContext");
+local VRService = game:GetService("VRService");
+local VirtualUser = game:GetService("VirtualUser");
+local RunService = game:GetService("RunService");
+local HttpService = game:GetService("HttpService");
+local HapticService = game:GetService("HapticService");
+local UserInputService = game:GetService("UserInputService");
+local CurrentCamera = workspace.CurrentCamera
+
